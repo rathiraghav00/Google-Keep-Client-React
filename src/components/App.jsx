0ca6e6bfed432c3099373afd4993bb53983d6174
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-04-05 02:30:28 +0530

    CRD working, need to convert JSON to array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import axios from 'axios'
 
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
+  useEffect(() => {
+    console.log("Use effect mein hun");
+    getAllNotes();
+  }, []);
+
+  const getAllNotes = () => {
+      axios.get('http://localhost:3000/notes')
+      .then((response) => {
+        console.log(response);
+        // var arr = JSON.parse(response.data);
+        // console.log(arr);
+        console.log(response.data);
+        
+        setNotes(response.data);
+      })
+      .catch(error => {
+        console.log("Error : ", error);
+      });
   }
 
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
+  function addNote(newNote) {
+    console.log("Yeh hai new note neeche");
+    console.log(newNote);
+    axios.post('http://localhost:3000/notes',{
+        email : "123@gmail.com",
+        title : newNote.title,
+        data: newNote.content,
+      }
+    )
+    .then((response) => {
+        console.log(response);
+        setNotes(response.data);
+      })
+      .catch(error => {
+        console.log("Error : ", error);
       });
-    });
+    
+    console.log("post requuest hogayi");
+
+    getAllNotes();
+  }
+
+  function deleteNote(title) {
+
+    axios.delete('http://localhost:3000/notes/' + title, {
+      headers : {
+        "Access-Control-Allow-Origin" : "*"
+      }
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log("Error : ", error);
+      });
+      
+      console.log("element deleted");
+
+      getAllNotes();
   }
 
   return (
@@ -30,8 +78,8 @@ function App() {
           <Note
             key={index}
             id={index}
-            title={noteItem.title}
-            content={noteItem.content}
+            title={noteItem.email}
+            content={noteItem.data}
             onDelete={deleteNote}
           />
         );
