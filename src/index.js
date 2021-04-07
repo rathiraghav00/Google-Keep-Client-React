0f0c90a8df19cc6616cfb6f9a82f124c7d271c1e
rathiraghav00 <42902117+rathiraghav00@users.noreply.github.com>
2021-04-07 15:36:09 +0530

    Adding localStorage to index.js didnot work

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import App from "./components/App";
 
-localStorage.clear();
+// localStorage.clear();
 
 ReactDOM.render(
   <div>
